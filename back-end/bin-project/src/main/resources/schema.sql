a6cbfe49dd6d4694003020783d97abe33f47a0c5
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-05-08 12:36:12 -0300

    Criação automática das tabelas

--- back-end/bin-project/src/main/resources/schema.sql
+++ back-end/bin-project/src/main/resources/schema.sql
@@ -1,53 +1,55 @@
-CREATE TABLE IF NOT EXISTS admin {
+DROP TABLE IF EXISTS admin, student, universe, world, phase, exercise, student_progress;
+
+CREATE TABLE IF NOT EXISTS admin (
     id INT PRIMARY KEY,
     username VARCHAR(200) NOT NULL,
     password VARCHAR(64) NOT NULL
-}
+);
 
 CREATE TABLE IF NOT EXISTS student (
     id INT PRIMARY KEY,
     username VARCHAR(200) NOT NULL,
     password VARCHAR(64) NOT NULL
-)
+);
 
-CREATE TABLE IF NOT EXISTS universe {
+CREATE TABLE IF NOT EXISTS universe (
     id INT PRIMARY KEY,
     name VARCHAR(200),
-    description VARCHAR(255)
-}
+    description TEXT
+);
 
-CREATE TABLE IF NOT EXISTS world {
+CREATE TABLE IF NOT EXISTS world (
     id INT PRIMARY KEY,
     name VARCHAR(150),
-    description (255),
+    description TEXT,
     universe_id INT NOT NULL,
     FOREIGN KEY (universe_id) REFERENCES universe(id)
-}
+);
 
-CREATE TABLE IF NOT EXISTS phase {
+CREATE TABLE IF NOT EXISTS phase (
     id INT PRIMARY KEY,
     name VARCHAR(150),
-    description (200),
+    description TEXT,
     concept_code INT,
-    world_id NOT NULL,
+    world_id INT NOT NULL,
     FOREIGN KEY (world_id) REFERENCES world(id)
-}
+);
 
-CREATE TABLE IF NOT EXISTS exercise {
+CREATE TABLE IF NOT EXISTS exercise (
     id INT PRIMARY KEY,
     concept_code INT,
-    question TEXT(10000) NOT NULL,
+    question TEXT NOT NULL,
     answers VARCHAR(10000) NOT NULL,
-    correct_answer VARCHAR(3000) NOT NULL,
-}
+    correct_answer VARCHAR(3000) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS student_progress {
+CREATE TABLE IF NOT EXISTS student_progress (
     id_phase INT NOT NULL,
     id_student INT NOT NULL,
-    hours_spent TIME,
+    hours_spent TIMESTAMP,
     FOREIGN KEY (id_phase) REFERENCES phase(id),
     FOREIGN KEY (id_student) REFERENCES student(id)
-}
+);
 
 
 
