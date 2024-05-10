fd1f67bdd6d7dbb3ea0493ec3142d3e9527e890a
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-05-10 17:32:54 -0300

    Criação das Exceções

---
+++ back-end/bin-project/src/main/java/com/binarying/binproject/exceptions/AdministratorNotFoundException.java
@@ -0,0 +1,13 @@
+package com.binarying.binproject.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AdministratorNotFoundException extends RuntimeException {
+    public AdministratorNotFoundException() {
+        super("Administrator Not Found!");
+    }
+}
--- back-end/bin-project/src/main/java/com/binarying/binproject/exceptions/ExerciseNotFoundException.java
+++ back-end/bin-project/src/main/java/com/binarying/binproject/exceptions/ExerciseNotFoundException.java
@@ -3,9 +3,11 @@ package com.binarying.binproject.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException() {
-        super("Data Not Found!");
+public class ExerciseNotFoundException extends RuntimeException {
+    public ExerciseNotFoundException() {
+        super("Exercise Not Found!");
     }
 }
