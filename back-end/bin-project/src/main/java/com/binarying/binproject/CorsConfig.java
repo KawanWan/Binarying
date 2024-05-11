abe8357c1ed3893bc0732d75d5885c1e5a7fbdb3
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-05-11 18:58:15 -0300

    Permissão de acesso Frontend

---
+++ back-end/bin-project/src/main/java/com/binarying/binproject/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.binarying.binproject;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Especifique o caminho da sua API
+                .allowedOrigins("http://localhost:5500") // URL permitida para acesso
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Métodos HTTP permitidos
+    }
+}
