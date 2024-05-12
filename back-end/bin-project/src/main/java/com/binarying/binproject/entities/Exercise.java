98b08c2b51e165df59d7244d749aad877a17e39a
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-05-11 23:20:31 -0300

    Alteração classe Exercise

--- back-end/bin-project/src/main/java/com/binarying/binproject/entities/Exercise.java
+++ back-end/bin-project/src/main/java/com/binarying/binproject/entities/Exercise.java
@@ -1,12 +1,18 @@
 package com.binarying.binproject.entities;
 
-import java.util.Map;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 import com.binarying.binproject.entities.enumerations.ExerciseType;
 
 public record Exercise(
-    Integer id,
-    String concept,
-    ExerciseType exerciseType,
-    Map<String, Boolean> answers 
-) {}
+        @Id Integer id,
+        String concept,
+        @Column("exercisetype")ExerciseType exerciseType,
+        String question,
+        String[] answers,
+        @Column("correctanswer")String correctAnswer,
+        @Column("phase_id") Integer phaseId
+
+) {
+}
